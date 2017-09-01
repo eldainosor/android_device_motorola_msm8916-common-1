ba392a19710cac16a1f9674265c342a1f8c36ca3
Alberto97 <albertop2197@gmail.com>
2017-09-01 16:19:56 +0200

    msm8916-common: Kill IMS
    Change-Id: I3163a077b745f4201661713a70c6aa61ad918896

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,11 +16,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := MediaBuffer.c
+LOCAL_SRC_FILES := MediaCodec.cpp
 
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libstagefright libmedia
 
-LOCAL_MODULE := libshims_ims
+LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
